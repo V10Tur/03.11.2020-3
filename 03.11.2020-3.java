d614ea3ca446886f6f066acfad9e6ea4a3832bfc
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:17:29 +0300

    Create 03.11.2020-3.java

---
+++ 03.11.2020-3.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Program {
+
+    public static void main (String args[]){
+
+        sum(1, 2, 3);           // 6
+        sum(1, 2, 3, 4, 5);     // 15
+        sum();                  // 0
+    }
+    static void sum(int ...nums){
+
+        int result =0;
+        for(int n: nums)
+            result += n;
+        System.out.println(result);
+    }
+
+    }
+
+
